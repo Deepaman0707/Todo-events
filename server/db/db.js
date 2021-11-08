d2239932e9c8be3d8d602fb2612cf41d389cf7d7
Aman Deep <deep.aman07.ad@gmail.com>
2021-11-08 17:43:05 +0530

    postgres setup done.

---
+++ server/db/db.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const { Pool } = require('pg')
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const pool = new Pool({ connectionString })
+
+module.exports = pool
