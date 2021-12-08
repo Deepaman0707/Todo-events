0d6ebf085a77ec3d95dce6ef0dc0ee903abe006a
Aman Deep <deep.aman07.ad@gmail.com>
2021-12-08 15:25:26 +0530

    Meta tag changed. Logo also changed.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react'
-import noteContext from '../context/noteContext'
+import React from 'react'
+import '../Styles/About.css'
 
 const About = () => {
-  const a = useContext(noteContext)
   return (
-    <div>
-      <h1>{a.name}</h1>
-      <h1>{a.class}</h1>
+    <div className='aboutBox'>
+      <h2>About</h2>
+      <p>
+        Are you tired by missing deadlines of the day? <br />
+        Time to say goodbye to all the missing deadlines. <br />
+        Start from today, start from now with Get Set Go that will make your day
+        more organized !
+      </p>
+      <p>
+        About me? Well, I am a self-taught programmer who is a tech enthusiast
+        and passionate about learning new things. <br />I can spend my whole day
+        with DSA, HTML, CSS, React and other web technologies. As a web
+        developer, I take care about creating amazing and useful website. For
+        me, coding can break a very big problem into fragments. Looking forward
+        to new opportunities !
+      </p>
     </div>
   )
 }
